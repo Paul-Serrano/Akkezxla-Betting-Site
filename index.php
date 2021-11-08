7bb6478f8529aeda5d7315783f67a789293272a5
Serrano Paul <paul.serrano08374@gmail.com>
2021-11-08 14:15:18 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,241 @@
+<?php
+
+if(isset($_POST['bet-form'])){
+
+        $header = "";
+        $header="MIME-Version: 1.0\r\n";
+        $header.='From:"Bet Site"'."\n";
+$header.='Content-Type:text/html; charset="utf-8"'."\n";
+$header.='Content-Transfer-Encoding: 8bit';
+$message='
+<html>
+  <body>
+    <div align="center">
+      <p>Ticket 1</p>
+      '.$_POST['match1'].' '.$_POST['match2'].' '.$_POST['match3'].' '.$_POST['match4'].' '.$_POST['match5'].'
+      <hr />
+      <p>Ticket 2:</p>
+      '.$_POST['match6'].' '.$_POST['match7'].' '.$_POST['match8'].' '.$_POST['match9'].' '.$_POST['match10'].'
+    </div>
+  </body>
+</html>
+'; 
+mail("paul.serrano08374@gmail.com", "Contact -- Portfolio !", $message,
+$header); 
+} 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"
+        integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Akkezxla 1-N-2</title>
+</head>
+
+<body>
+    <main>
+        <h1>Akkezxla Ligue 1</h1>
+        <section>
+            <div class="form-container">
+                <form class="form" action="" method="POST">
+                    <div class="name-block">
+                        <input class="name" type="text" name="name" placeholder="Your Name !" />
+                    </div>
+                    <div class="form-content">
+                        <div class="ticket" id="ticket1">
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match1-1" name="match1" type="radio" value="1" />
+                                    <label for="match1-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match1-N" name="match1" type="radio" value="N" />
+                                    <label for="match1-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match1-2" name="match1" type="radio" value="2" />
+                                    <label for="match1-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match2-1" name="match2" type="radio" value="1" />
+                                    <label for="match2-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match2-N" name="match2" type="radio" value="N" />
+                                    <label for="match2-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match2-2" name="match2" type="radio" value="2" />
+                                    <label for="match2-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match3-1" name="match3" type="radio" value="1" />
+                                    <label for="match3-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match3-N" name="match3" type="radio" value="N" />
+                                    <label for="match3-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match3-2" name="match3" type="radio" value="2" />
+                                    <label for="match3-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match4-1" name="match4" type="radio" value="1" />
+                                    <label for="match4-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match4-N" name="match4" type="radio" value="N" />
+                                    <label for="match4-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match4-2" name="match4" type="radio" value="2" />
+                                    <label for="match4-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match5-1" name="match5" type="radio" value="1" />
+                                    <label for="match5-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match5-N" name="match5" type="radio" value="N" />
+                                    <label for="match5-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match5-2" name="match5" type="radio" value="2" />
+                                    <label for="match5-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="final-bet">
+                            <div class="result-bet"></div>
+                        </div>
+                        <div class="ticket" id="ticket2">
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match6-1" name="match6" type="radio" value="1" />
+                                    <label for="match6-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match6-N" name="match6" type="radio" value="N" />
+                                    <label for="match6-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match6-2" name="match6" type="radio" value="2" />
+                                    <label for="match6-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match7-1" name="match7" type="radio" value="1" />
+                                    <label for="match7-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match7-N" name="match7" type="radio" value="N" />
+                                    <label for="match7-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match7-2" name="match7" type="radio" value="2" />
+                                    <label for="match7-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match8-1" name="match8" type="radio" value="1" />
+                                    <label for="match8-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match8-N" name="match8" type="radio" value="N" />
+                                    <label for="match8-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match8-2" name="match8" type="radio" value="2" />
+                                    <label for="match8-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match9-1" name="match9" type="radio" value="1" />
+                                    <label for="match9-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match9-N" name="match9" type="radio" value="N" />
+                                    <label for="match9-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match9-2" name="match9" type="radio" value="2" />
+                                    <label for="match9-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="match-block">
+                                <p class="match">Paris -- Marseille</p>
+                                <div class="bet-block">
+                                    <input id="match10-1" name="match10" type="radio" value="1" />
+                                    <label for="match10-1">
+                                        <p>1</p>
+                                    </label>
+                                    <input id="match10-N" name="match10" type="radio" value="N" />
+                                    <label for="match10-N">
+                                        <p>Nul</p>
+                                    </label>
+                                    <input id="match10-2" name="match10" type="radio" value="2" />
+                                    <label for="match10-2">
+                                        <p>2</p>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" name="bet-form" class="btn btn-light form-btn">Submit</button>
+                </form>
+            </div>
+        </section>
+    </main>
+    <script src="script.js"></script>
+</body>
+
+</html>
